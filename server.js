5d56a42f872c78bd42da19f8b0ac90c0a43271ba
jawwwr <romierlodi@gmail.com>
2018-10-05 16:17:14 +0800

    Add socket to server

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const http = require('http');
 const app = require('./app');
+const io = require('socket.io')(server);
 
 const port = process.env.PORT || 3001;
 
 const server = http.createServer(app);
+
+io.on('some event name here', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+    });
+})
 
 server.listen(port);
